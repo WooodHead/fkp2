3cf5ba534c52d494eaf3cb0ca73cfe09f99075b7
yc <kixi@163.com>
2016-12-18 14:10:38 +0800

    node: db模块增加自增变量

--- server/db/mongo/index.js
+++ server/db/mongo/index.js
@@ -1,15 +1,40 @@
 let libs = require('libs')
 let fs = require('fs')
 let path = require('path')
+let mongoose = require("mongoose")
+let Schema = mongoose.Schema
 
-function _db(fkp){
-  this.fkp = fkp
+// 用户自增id, seq为自增变量
+// 如果其他文档需要自增id的字段，在u_seq后增加
+var IncreaseSchema = new Schema({
+  _id: {type: String, required: true},
+  u_seq: { type: Number, default: 0 }
+});
+
+let connectState
+async function connect(){
+  try {
+    if (connectState) return true
+    connectState = await require('./common/connect').default()
+    if (connectState) {
+      var Increase = mongoose.model('Increase', IncreaseSchema);
+      let _id = await Increase.findOne({_id: 'Increase'}).exec()
+      if (!_id) {
+        new Increase({_id: 'Increase', u_seq: 0}).save()
+      }
+      return connectState
+    } else {
+      throw '数据库没有连接'
+    }
+} catch (e) {
+    console.log(e)
+    return false
+  }
 }
 
 export default async function(fkp, folder, requiredFolder){
-  let connectState = await require('./common/connect').default(folder)
   try {
-    if (!connectState) throw '数据库没有连接'
+    await connect()
     let $folder = await fkp.fileexist(folder)
     if ($folder.isDirectory()) {
       let dirs = (await fkp.readdir(folder) || []).filter((x) => Object.values(requiredFolder).includes(x))
@@ -26,6 +51,10 @@ export default async function(fkp, folder, requiredFolder){
     console.log(e)
     return false
   }
+}
+
+function _db(fkp){
+  this.fkp = fkp
 }
 
 // 注册model
