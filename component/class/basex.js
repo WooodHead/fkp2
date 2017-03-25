81caa958d91cc700ea527fb25afd139ca0688299
yc <kixi@163.com>
2017-03-25 18:40:52 +0800

    组件 组件类basex，combinex的基类

--- component/class/basex.js
+++ component/class/basex.js
@@ -1,4 +1,5 @@
 import {inject} from 'libs'
+import combineX from 'component/mixins/combinex'
 
 // BaseCombine
 export default class  {
@@ -7,8 +8,17 @@ export default class  {
     this.client = (() => typeof window !== 'undefined')()
     this.element
     this.inject = this::this.inject
+    this.combinex = this::this.combinex
 
     this.inject()
+  }
+
+  combinex(GridsBase, Actions){
+    const CombX = combineX(GridsBase, Actions)
+    this.x = CombX.element
+    this.dispatch = CombX.dispatch
+    this.setActions = CombX.saxer.setActions
+    this.roll = CombX.saxer.roll
   }
 
   inject(src){
@@ -21,6 +31,25 @@ export default class  {
         src(ij)
       }
       return ij
+    }
+  }
+
+  render(id){
+    const X = this.x
+    if (this.config.props) {
+      if (id) {
+        if (typeof id == 'string') {
+          React.render(<X {...this.config.props}/>, document.getElementById(id))
+        } else if (typeof id == 'object' && id.nodeType) {
+          React.render(<X {...this.config.props}/>, id)
+        } 
+      } else if (this.config.container) {
+        React.render(<X {...this.config.props}/>, document.getElementById(this.config.container))
+      } else {
+        return <X {...this.config.props}/>
+      }
+    } else {
+      return X
     }
   }
 }
