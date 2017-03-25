149402bc402977e7774b19ea99ed8ea6b76ac1c2
yc <kixi@163.com>
2017-03-25 15:19:13 +0800

    组件 高阶方法， storehlc优化

--- component/mixins/storehlc.js
+++ component/mixins/storehlc.js
@@ -3,20 +3,14 @@ export default (id, ComposedComponent) => {
   try {
     if (typeof SAX == undefined) throw 'storehlc depend on SAX, SAX is fkp-sax, is a Global fun'
     if (!id) throw 'storehlc id must be set'
-
     return class extends ComposedComponent {
       constructor(props) {
         super(props)
-        this.state.globalName = id
-      }
-
-      componentWillMount() {
         SAX.bind(id, this)
-        super.componentWillMount()
+        this.state.globalName = id
       }
     }
   } catch (e) {
-    // console.log(e);
     return ComposedComponent
   }
 }
