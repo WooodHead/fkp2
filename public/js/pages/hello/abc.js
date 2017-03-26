242a8a13400c2c3f0f332a12c34632017faf47b0
yc <kixi@163.com>
2017-03-27 02:39:00 +0800

    agzgz: pages/hello 样式优化

--- public/js/pages/hello/abc.js
+++ public/js/pages/hello/abc.js
@@ -7,6 +7,7 @@ import scrollHlc from 'component/mixins/scrollhlc'
 var ws = require('libs/wsocket')
 require('./_common/pushboom')(ws)
 
+const autoinject = false;
 // websocket
 // ws.emit('hello', 'hi')
 // ws.on('hello', function(val){
@@ -22,63 +23,26 @@ mediaQuery({
   }
 })
 
-const StickyBBox = sticky.bottom(document.getElementById('agzgzNav'), {delay: 2000, autoinject: false})
+inject().css(
+`
+  .navtop{
+    width: 100%;
+    height: 50px;
+    padding: 5px;
+    background-color: #318853;
+  }
+`
+)
 
-let Xslider = Slider({
-  container: 'slider',
-  data: [
-    <div className="pure-g">
-      <div className="pure-u-8-12">
-        <img src="/images/banner/hello/banner-3.jpg" title="Funky roots" />
-      </div>
-      <div className="pure-u-4-12 descript">
-        <h3>FKP2</h3>
-        Full Stack Plus 2<br/> SAP/MPA最佳实践方式
-      </div>
-    </div>,
-
-    <div className="pure-g">
-      <div className="pure-u-8-12">
-        <img src="/images/banner/hello/banner-2.jpg" title="The long and winding road" />
-      </div>
-      <div className="pure-u-4-12 descript">
-        <h3>脚手架</h3>
-        gulp+webpack组合，灵活、模块化的脚手架系统
-      </div>
-    </div>,
-
-    <div className="pure-g">
-      <div className="pure-u-8-12">
-        <img src="/images/banner/hello/banner-7.jpg" title="Happy trees" />
-      </div>
-      <div className="pure-u-4-12 descript">
-        <h3>前端</h3>
-        Babel、JQ+React的混合流组件模式
-      </div>
-    </div>,
-
-    <div className="pure-g">
-      <div className="pure-u-8-12">
-        <img src="/images/banner/hello/banner-4.jpg" title="Happy trees" />
-      </div>
-      <div className="pure-u-4-12 descript">
-        <h3>node端</h3>
-        灵活、低配置，无限层级RESTFUL路由
-      </div>
+const nevTop = (
+  <div className='navtop'>
+    <div style={{width: '1050px',margin:'0 auto', height: '100%'}}>
+      <img src="/images/logo118.png" style={{height: '100%',width:'auto'}}/>
     </div>
-  ],
-  // control: [
-  //   <img src="/images/test/t_tree_root.jpg" title="Funky roots" />,
-  //   <img src="/images/test/hill_fence.jpg" title="The long and winding road" />,
-  //   <img src="/images/test/t_houses.jpg" title="Happy trees" />
-  // ],
-}).render()
-
-// const titles = [
-//   <a title="列表，如果所有元素都是列表">列表，如果所有元素都是列表</a>,
-//   <a>元件，组件是怎么构成的</a>,
-//   <a>元件，可伸缩的元件设计</a>
-// ]
+  </div>
+)
+const StickyBBox = sticky.bottom(document.getElementById('agzgzNav'), {delay: 2000, autoinject: false})
+const StickyTBox = sticky(nevTop, {autoinject: false})
 
 const titles1 = [
   <a className="iconfont icon-text" href='/docs' title="FKP2文档">FKP2文档</a>,
@@ -134,20 +98,18 @@ const titles1 = [
 ]
 const list1 = baselist({
   data: titles1,
-  autoinject: false,
-  listClass: 'books'
+  listClass: 'books',
+  autoinject
 })
 
 const floor1 = grids({
+  autoinject,
+  container: 'test',
   data: [
     {width: '100%', content: <h3 style={{color:'#9c9c9c'}}>推荐文章</h3>},
     {width: '100%', content: list1.render()}
   ],
-  autoinject: false,
-  container: 'test'
-})
-
-floor1.render()
+}).render()
 
 setTimeout(()=>{
   scrollHlc($('.columContainer')[0], {
