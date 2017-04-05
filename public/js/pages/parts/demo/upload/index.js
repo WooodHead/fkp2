3878650729e185b7ffb30855b17be00862b197ed
yc <kixi@163.com>
2017-04-05 16:34:43 +0800

    demo 上传组件demo未完成

---
+++ public/js/pages/parts/demo/upload/index.js
@@ -0,0 +1,44 @@
+var libs = require('libs/libs')
+
+//支持uploader.button
+//支持uploader.thumb
+//支持uploader.custom
+// var Uploader = require('modules/upload/index')
+var Uploader = require('component/client/uploader')
+
+// 支持inject.css
+// 支持inject.js
+var inject = libs.inject()
+
+
+
+//注入上传的button按钮样式
+inject.css(
+    ['/css/t/upload/button.css', 'upload_button']
+    , render_upload_button
+)
+
+
+function render_upload_button(){
+    Uploader.custom(
+        function(){
+            var me = this;
+            $('#bbcc').click(function(){
+                me.trigger('upfile')
+            })
+        },
+        //上传成功后的回调
+        function(){
+          console.log('======== 完成')
+            // libs.msgtips('不错啊')
+        }
+    )
+
+    Uploader.button('upload', function(){
+        libs.msgtips('上传成功')
+    })
+
+    Uploader.thumb('thumb', '带缩略图', function(){
+        libs.msgtips('上传成功')
+    })
+}
