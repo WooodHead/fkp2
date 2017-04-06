7fe508e76fa9900dd5ecdb3fbcc3955935195832
yc <kixi@163.com>
2017-04-06 18:27:08 +0800

    组件 uploader未完成

--- component/modules/upload/index.js
+++ component/modules/upload/index.js
@@ -1,6 +1,18 @@
 var libs = require('libs')
 
 let uploaderer
+let uploader
+
+// 优化retina, 在retina下这个值是2
+var ratio = window.devicePixelRatio || 1,
+
+  // 缩略图大小
+  thumbnailWidth = 100 * ratio,
+  thumbnailHeight = 100 * ratio;
+
+
+// 上传样式类型
+function noon(){};
 
 function uploadAction2(btn, title, callback, type){
   if (typeof title === 'function'){
@@ -14,149 +26,116 @@ function uploadAction2(btn, title, callback, type){
   if (typeof btn === 'string'){
     var $list = $('#'+btn).siblings()
   }
-  // 优化retina, 在retina下这个值是2
-  var ratio = window.devicePixelRatio || 1,
 
-    // 缩略图大小
-    thumbnailWidth = 100 * ratio,
-    thumbnailHeight = 100 * ratio,
+  var uploader_style = {
+    thumb: function(file){
+      var $li = $(
+          '<div id="' + file.id + '" class="file-item thumbnail">' +
+              '<img>' +
+              '<div class="info">' + file.name + '</div>' +
+          '</div>'
+        ),
+        $img = $li.find('img');
+        $list.append( $li );
 
-    // Web Uploader实例
-    uploader;
+        // 创建缩略图
+        uploader.makeThumb( file, function( error, src ) {
+          if ( error ) {
+            $img.replaceWith('<span>不能预览</span>');
+            return;
+          }
+          $img.attr( 'src', src );
+        }, thumbnailWidth, thumbnailHeight );
+    },
+    button: noon
+  }
 
-    // 初始化Web Uploader
-    uploader = webUploader.create({
-      // 自动上传。
-      auto: true,
+  uploader.on('beforeFileQueued', function( file ){
+    // file.name = libs.guid()+'.'+file.ext;
+  })
 
-      // swf文件路径
-      swf: '/images/Uploader.swf',
+  // 当有文件添加进来的时候
+  uploader.on( 'fileQueued', function( file ) {
+    uploader_style[me.uploader_type].call(uploader, file)
+  });
 
-      // 文件接收服务端。
-      server: '/upup',
+  // 文件上传过程中创建进度条实时显示。
+  uploader.on( 'uploadProgress', function( file, percentage ) {
+    var $li = $( '#'+file.id ),
+      $percent = $li.find('.progress span');
 
-      // 只允许选择文件，可选。
-      accept: {
-        title: 'Images',
-        extensions: 'gif,jpg,jpeg,png',
-        mimeTypes: 'image/*'
-      }
-    });
-
-    // 上传样式类型
-    function noon(){};
-
-    var uploader_style = {
-      thumb: function(file){
-        var $li = $(
-            '<div id="' + file.id + '" class="file-item thumbnail">' +
-                '<img>' +
-                '<div class="info">' + file.name + '</div>' +
-            '</div>'
-          ),
-          $img = $li.find('img');
-          $list.append( $li );
-
-          // 创建缩略图
-          uploader.makeThumb( file, function( error, src ) {
-            if ( error ) {
-              $img.replaceWith('<span>不能预览</span>');
-              return;
-            }
-            $img.attr( 'src', src );
-          }, thumbnailWidth, thumbnailHeight );
-      },
-      button: noon
+    // 避免重复创建
+    if ( !$percent.length ) {
+      $percent = $('<p class="progress"><span></span></p>')
+        .appendTo( $li )
+        .find('span');
     }
 
-    uploader.on('beforeFileQueued', function( file ){
-      // file.name = libs.guid()+'.'+file.ext;
+    $percent.css( 'width', percentage * 100 + '%' );
+  });
+
+  // 文件上传成功，给item添加成功class, 用样式标记上传成功。
+  uploader.on( 'uploadSuccess', function( file, ret ) {
+    $( '#'+file.id ).addClass('upload-state-done');
+    if(callback && ret.state=='success'){
+      callback(file, ret);
+    }
+  });
+
+  // 文件上传失败，现实上传出错。
+  uploader.on( 'uploadError', function( file, reason ) {
+    var $li = $( '#'+file.id ),
+        $error = $li.find('div.error');
+
+    // 避免重复创建
+    if ( !$error.length ) {
+      $error = $('<div class="error"></div>').appendTo( $li );
+    }
+
+    $error.text('上传失败');
+
+    alert('上传失败')
+  });
+
+  // 完成上传完了，成功或者失败，先删除进度条。
+  uploader.on( 'uploadComplete', function( file ) {
+    $( '#'+file.id ).find('.progress').remove();
+    file.name = file.name.split("&&&")[1];
+  });
+
+
+  if (typeof btn === 'string'){
+    $('#'+btn).mouseenter(function(){
+      uploader.refresh();
     })
 
-    // 当有文件添加进来的时候
-    uploader.on( 'fileQueued', function( file ) {
-      uploader_style[me.uploader_type].call(uploader, file)
-    });
+    uploader.addButton({
+      id:  '#'+btn,
+      innerHTML: title||'选择文件'
+    })
+  }
+  else
+  if (typeof btn === 'function'){
+    //自定义上传样式
+    //1、先插入一个 file 控件
+    var _file = $('<input style="position:absolute;clip:rect(1px 1px 1px 1px);left:0;top:0 " type="file"></input>')
+    $('body').append(_file)
 
-    // 文件上传过程中创建进度条实时显示。
-    uploader.on( 'uploadProgress', function( file, percentage ) {
-      var $li = $( '#'+file.id ),
-        $percent = $li.find('.progress span');
+    //h5页面可用
+    _file.change(function(){
+      // var fileobj = this.files[0]
+      uploader.addFile(this.files)
+    })
 
-      // 避免重复创建
-      if ( !$percent.length ) {
-        $percent = $('<p class="progress"><span></span></p>')
-          .appendTo( $li )
-          .find('span');
+    uploader.on('upfile', function(cb){
+      if (cb && typeof cb === 'function'){
+        callback = cb
       }
+      _file.click();
+    })
 
-      $percent.css( 'width', percentage * 100 + '%' );
-    });
-
-    // 文件上传成功，给item添加成功class, 用样式标记上传成功。
-    uploader.on( 'uploadSuccess', function( file, ret ) {
-      $( '#'+file.id ).addClass('upload-state-done');
-      if(callback && ret.state=='success'){
-        callback(file, ret);
-      }
-    });
-
-    // 文件上传失败，现实上传出错。
-    uploader.on( 'uploadError', function( file, reason ) {
-      var $li = $( '#'+file.id ),
-          $error = $li.find('div.error');
-
-      // 避免重复创建
-      if ( !$error.length ) {
-        $error = $('<div class="error"></div>').appendTo( $li );
-      }
-
-      $error.text('上传失败');
-
-      alert('上传失败')
-    });
-
-    // 完成上传完了，成功或者失败，先删除进度条。
-    uploader.on( 'uploadComplete', function( file ) {
-      $( '#'+file.id ).find('.progress').remove();
-      file.name = file.name.split("&&&")[1];
-    });
-
-
-    if (typeof btn === 'string'){
-      $('#'+btn).mouseenter(function(){
-        uploader.refresh();
-      })
-
-      uploader.addButton({
-        // 选择文件的按钮。可选。
-        // 内部根据当前运行是创建，可能是input元素，也可能是flash.
-        id:  '#'+btn,
-        innerHTML: title||'选择文件'
-      })
-    }
-    else
-    if (typeof btn === 'function'){
-      //自定义上传样式
-      //1、先插入一个 file 控件
-      var _file = $('<input style="position:absolute;clip:rect(1px 1px 1px 1px);left:0;top:0 " type="file"></input>')
-      $('body').append(_file)
-
-      //h5页面可用
-      _file.change(function(){
-        // var fileobj = this.files[0]
-        uploader.addFile(this.files)
-      })
-
-      uploader.on('upfile', function(cb){
-        if (cb && typeof cb === 'function'){
-          callback = cb
-        }
-        _file.click();
-      })
-
-      btn.call(uploader)
-    }
+    btn.call(uploader)
+  }
 }
 
 function render_uploader(name, title, cb, type){
@@ -164,7 +143,6 @@ function render_uploader(name, title, cb, type){
     cb = title;
     title = false;
   }
-
   var exist_id = document.getElementById(name)
   if (exist_id){
     var _id = libs.guid('uploader')
@@ -179,7 +157,7 @@ function render_uploader(name, title, cb, type){
 
 function preUpload(cb){
   libs.inject().js('/js/t/webuploader.js', function(){
-    uploaderer = webUploader.create({
+    uploader = WebUploader.create({
       // 自动上传。
       auto: true,
 
@@ -196,6 +174,7 @@ function preUpload(cb){
         mimeTypes: 'image/*'
       }
     });
+    cb()
   })
 }
 
--- component/modules/uploader/index.js
+++ component/modules/uploader/index.js
@@ -2,25 +2,35 @@ import {inject} from 'libs'
 import BaseX from 'component/class/basex'
 // import wrapItem from 'component/mixins/combinex'
 
+var ratio = window.devicePixelRatio || 1,
+  // 缩略图大小
+  thumbnailWidth = 100 * ratio,
+  thumbnailHeight = 100 * ratio
 
 class thumbUp extends React.Component {
   constructor(props){
     super(props)
     this.state = {
-      data: []
+      data: []  // item => {id: file.id, name: file.name, src: imgurl, progress: 0, stat: 'success/faild'}
     }
   }
   render(){
-    const imglist = this.state.data.map( (url, ii) => <li><img key={"preview"+ii} src={url}/></li> )
-
+    const imglist = this.state.data.map( (item, ii) => {
+      const progress = <span className='up-progress' style={{width: item.progress+'%'}}></span>
+      return (
+        <li key={"preview"+ii}>
+          <img src={item.src}/>
+          <div className="up-info">{item.name}</div>
+          {progress}
+        </li>
+      )
+    })
     return (
       <div className='uploader-wrap'>
         <div className='uploader-list'>
-          <ul>
-            {imglist}
-          </ul>
+          { imglist.length ? <ul> {imglist} </ul> : '' }
         </div>
-        <div className='uploader-button'>上传文件</div>
+        <div ref={'upBtn'} className='uploader-button'>上传文件</div>
       </div>
     )
   }
@@ -28,8 +38,47 @@ class thumbUp extends React.Component {
 
 const Actions = {
   APPEND: function(state, options){
-    const this.curState.data = this.curState.data.push(options.url)
+    // this.curState.data = this.curState.data.push(options)
+    this.curState.data.push(options)
     return this.curState
+  },
+
+  PROGRESS: function(state, options){
+    const data = this.curState.data
+    const targetIndex = _.findIndex(data, function(o) { return o.id == options.id });
+    if (targetIndex > -1) {
+      let target = data[targetIndex]
+      target['progress'] = options.progress
+      this.curState.data[targetIndex] = target
+    }
+    return this.curState
+  },
+
+  STAT: function(state, options){
+    const data = this.curState.data
+    const targetIndex = _.findIndex(data, function(o) { return o.id == options.id });
+    if (targetIndex > -1) {
+      let target = data[targetIndex]
+      target['stat'] = options.stat
+      this.curState.data[targetIndex] = target
+    }
+    return this.curState
+  }
+}
+
+
+function idfMethod(context){
+  const app = context
+  return function(dom, intent){
+    const self = this
+    app.on('DefaultMethod', function(){
+      const uploader = context.uploader
+      const btn = self.refs['upBtn']
+      uploader.addButton({
+        id:  btn
+        // innerHTML: '选择文件'
+      })
+    })
   }
 }
 
@@ -39,90 +88,70 @@ class App extends BaseX {
     super(config)
     this.uploader
     this.combinex(thumbUp, Actions)
+    if (this.config.props) {
+      this.config.props['itemDefaultMethod'] = idfMethod(this)
+    } else {
+      this.config.props = {
+        itemDefaultMethod: idfMethod(this)
+      }
+    }
+    this.injectStatic()
+  }
+
+  injectStatic(){
+    const that = this
     inject().js('/js/t/webuploader.js', ()=>{
-      this.uploader = webUploader.create(this.config.uploaderConfig)
+      this.uploader = WebUploader.create(this.config.uploaderConfig)
+      const uploader = this.uploader
+      uploader.on('beforeFileQueued', function( file ){ })
+      uploader.on('fileQueued', function( file ) {
+        uploader.makeThumb( file, function( error, src ) {
+          that.append(file, src)
+        }, thumbnailWidth, thumbnailHeight );
+      })
+
+      uploader.on( 'uploadProgress', function( file, percentage ) {
+        that.progress(file, percentage)
+      });
+
+      uploader.on( 'uploadSuccess', function( file, ret ) {
+        that.stat(file, 'success')
+        if (typeof that.config.success == 'function') {
+          that.config.success(file, ret)
+        }
+      });
+
+      uploader.on( 'uploadError', function( file, reason ) {
+        that.stat(file, 'faild')
+        if (typeof that.config.faild == 'function') {
+          that.config.faild(file, reason)
+        }
+      });
+      that.roll('DefaultMethod', {})
     })
   }
+
+  stat(file, ret){
+    if (ret == 'success') this.dispatch('STAT', {id: file.id, name: file.name, stat: 'success'})
+    this.dispatch('STAT', {id: file.id, name: file.name, stat: 'faild'})
+  }
+
+  append(file, imgurl){
+    this.dispatch('APPEND', {id: file.id, name: file.name, src: imgurl, progress: 0})
+  }
+
+  progress(file, percentage){
+    this.dispatch('PROGRESS', {id: file.id, name: file.name, progress: percentage})
+  }
+
 }
 
-// function button(name, title, cb){
-//   preUpload(function(){
-//     return render_uploader(name, title, cb, 'button')
-//   })
-// }
-//
-// function thumb(name, title, cb){
-//   preUpload(function(){
-//     return render_uploader(name, title, cb, 'thumb')
-//   })
-// }
-//
-// function custom(name, cb){
-//   preUpload(function(){
-//     return new uploadAction2(name, false, cb, 'button')
-//   })
-// }
-
-// class upldClass {
-//   constructor(config) {
-//     this.config = config
-//     this.injected = false
-//     this.uploader
-//     this.injectStatic()
-//   }
-//
-//   injectStatic(){
-//     inject().js('/js/t/webuploader.js', ()=>{
-//       this.injected = true
-//       this.uploader = webUploader.create(this.config.uploaderConfig)
-//     })
-//   }
-//
-//   thumb(){
-//     const that = this
-//     const thumbElement = wrapItem(
-//       <div className='uploader-wrap'>
-//         <div className='uploader-list'></div>
-//         <div className='uploader-button'>上传文件</div>
-//       </div>
-//       , function(dom){
-//         const uploader = this.uploader
-//         if (uploader) {
-//           const $list = $(dom).find('.uploader-list')
-//
-//           uploader.on('beforeFileQueued', function( file ){ })
-//           uploader.on( 'fileQueued', function( file ) {
-//             var $li = $(
-//                 '<div id="' + file.id + '" class="file-item thumbnail">' +
-//                     '<img>' +
-//                     '<div class="info">' + file.name + '</div>' +
-//                 '</div>'
-//               ),
-//               $img = $li.find('img');
-//               $list.append( $li );
-//
-//               // 创建缩略图
-//               uploader.makeThumb( file, function( error, src ) {
-//                 if ( error ) {
-//                   $img.replaceWith('<span>不能预览</span>');
-//                   return;
-//                 }
-//                 $img.attr( 'src', src );
-//               }, thumbnailWidth, thumbnailHeight );
-//           });
-//           uploader.on( 'uploadProgress', function( file, percentage ) { });
-//           uploader.on( 'uploadSuccess', function( file, ret ) { });
-//           uploader.on( 'uploadError', function( file, reason ) { });
-//           uploader.on( 'uploadComplete', function( file ) { });
-//         }
-//       }
-//     )
-//   }
-//
-// }
 
 function upld(opts){
-  const dft = {
+  let dft = {
+    props: false,
+    success: function(){},
+    faild: function(){},
     uploaderConfig: {
       // 自动上传。
       auto: true,
@@ -137,7 +166,8 @@ function upld(opts){
       accept: {
         title: 'Images',
         extensions: 'gif,jpg,jpeg,png',
-        mimeTypes: 'image/*'
+        // mimeTypes: 'image/*'
+        mimeTypes: 'image/jpg, image/jpeg, image/png, image/gif'
       }
     }
   }
@@ -149,7 +179,7 @@ function upld(opts){
 }
 
 module.exports = {
-  button: button,
-  thumb: thumb,
-  custom: custom
+  // button: button,
+  // custom: custom
+  thumb: upld
 };
