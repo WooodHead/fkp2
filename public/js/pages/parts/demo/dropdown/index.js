71c05ebc906697560157baefdde956d867b24ad1
yc <kixi@163.com>
2017-04-08 18:12:42 +0800

    Demo: dropdown下拉菜单调整

--- public/js/pages/parts/demo/dropdown/index.js
+++ public/js/pages/parts/demo/dropdown/index.js
@@ -1,7 +1,15 @@
+import {inject} from 'libs'
 import { dropdown } from 'component/client'
 
+inject().css(`
+  #demo_dd{
+    width: 200px;
+    margin: 1em;
+  }
+`)
+
 const data = [
-  {title: 'xxx'},
+  {title: <div>123<span>456</span></div>},
   {title: 'yyy'},
   {title: 'zzz'},
   {title: 'aaa'}
@@ -9,10 +17,12 @@ const data = [
 
 const xxx = dropdown({
   data: data,
+  listClass: 'dropdown-item dropdown-button',
   container: 'demo_dd',
+  placeholder: '请选择',
   itemMethod: function(dom){
     const ctx = this
-    $(this).click(function(e){
+    $(dom).click(function(e){
       e.stopPropagation()
       ctx.text(this.innerHTML)
       ctx.value(this.innerHTML)
